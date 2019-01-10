4a4deaa2506f33ad9f4a4ae738c5c1aa8f72e79b
Abishek <apotlapalli@drtstrategies.com>
2019-01-10 15:24:53 -0500

    COmmit Done

--- src/main/java/abi1/test1/App.java
+++ src/main/java/abi1/test1/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!...My first Maven Project!!!!" );
+        System.out.println( "Hello World!...My first Maven Project and im commiting to Git!!!!" );
     }
 }
