4021db1e2eeeeb125d1248d4b6d892390b2c5060
Abishek <apotlapalli@drtstrategies.com>
2019-01-12 20:28:16 -0500

    corrected code as recommended by sonarqube

--- src/main/java/abi1/test1/App.java
+++ src/main/java/abi1/test1/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!...My first Maven Project and im commiting to Git!!!!" );
+        //System.out.println( "Hello World!...My first Maven Project and im commiting to Git!!!!" );
+        logger.log("Hello World!...My first Maven Project and im commiting to Git!!!!");
     }
 }
